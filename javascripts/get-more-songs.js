c3d809839a28d3e4279890b918efecf7499effd4
Sally Fisher <sqfisher@gmail.com>
2015-08-05 09:39:03 -0500

    Modified modules for retrieving song JSON data to fix ES6 object return issue, which also corrected issue where JSON files were loaded twice

--- javascripts/get-more-songs.js
+++ javascripts/get-more-songs.js
@@ -1,21 +1,15 @@
-define(function getMoreSongs(callback) {
-
-  // $.ajax({
-  //       url: "songs2.json"
-  //     }).done(function(data) {
-  //       callback.call(this, data.songs);
-  //     });
-
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      var data = JSON.parse(xhr.responseText);
-      callback.call(this, data.songs);
-    }
-  };
-  xhr.open("GET", 'javascripts/songs2.json');
-  xhr.send();
+define(function() {
   return {
-    getMoreSongs
+    getMoreSongs: function (callback) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", 'javascripts/songs2.json');
+      xhr.send();
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          var data = JSON.parse(xhr.responseText);
+          callback.call(this, data.songs);
+        }
+      };
+    }
   };
 });
--- javascripts/populate-songs.js
+++ javascripts/populate-songs.js
@@ -2,16 +2,14 @@ define(function() {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", 'javascripts/songs.json');
   xhr.send();
-    
-  function getSongs(callback) {
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        var data = JSON.parse(xhr.responseText);
-        callback.call(this, data.songs);
-      }
-    };
-  }
   return {
-    getSongs
+    getSongs: function (callback) {
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          var data = JSON.parse(xhr.responseText);
+          callback.call(this, data.songs);
+        }
+      };
+    }
   };
 });
